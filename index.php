6fc3a1da1b1be202dda03b9d71b7209c41a548b2
Nikita Kot <pi2h@seznam.cz>
2017-02-23 16:58:28 +0100

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$a='b';
+
+$$a='30';
+
+echo $b;
+
+echo 'sssssssss'
+
+//pomoci forciklu vypsat tabulku + seznam
+?>
+
+
+<p>LLLLLLLLLLLOOOOOOOOOOOLLLLLLLLLLL</p>
+</body>
+</html>
+
