a556cad32440f2afecdeb7865da218e459c196bf
Nikita Kot <pi2h@seznam.cz>
2017-03-17 00:16:26 +0100

    du 4

--- du4/Employee.php
+++ du4/Employee.php
@@ -8,6 +8,43 @@
  */
 class Employee
 {
+private $jmeno;
+private $prijmeni;
+private $pohlavi;
+private $ulice;
+private $obec;
+private $psc;
+private $telefon;
+private $email;
+private $pozice;
+private $nadrizeny;
+
+    /**
+     * Employee constructor.
+     * @param $jmeno
+     * @param $prijmeni
+     * @param $pohlavi
+     * @param $ulice
+     * @param $obec
+     * @param $psc
+     * @param $telefon
+     * @param $email
+     * @param $pozice
+     * @param $nadrizeny
+     */
+    public function __construct($jmeno, $prijmeni, $pohlavi, $ulice, $obec, $psc, $telefon, $email, $pozice, $nadrizeny)
+    {
+        $this->jmeno = $jmeno;
+        $this->prijmeni = $prijmeni;
+        $this->pohlavi = $pohlavi;
+        $this->ulice = $ulice;
+        $this->obec = $obec;
+        $this->psc = $psc;
+        $this->telefon = $telefon;
+        $this->email = $email;
+        $this->pozice = $pozice;
+        $this->nadrizeny = $nadrizeny;
+    }
 
 
 }
